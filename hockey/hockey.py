996b0551bd67b1cc7f3fa8c85947a732b1f2b702
Elektra-V <vedika.chauhan@outlook.com>
2023-03-17 20:07:41 +0530

    Add break condition

--- hockey/hockey.py
+++ hockey/hockey.py
@@ -8,25 +8,29 @@ def main():
     session = requests.Session()
     
     teams = []
+    index = 1
+    while True:
+        response = session.get(url=f"https://www.scrapethissite.com/pages/forms/?page_num={index}&per_page=25")
+        html = response.text
 
-    response = session.get(url="https://www.scrapethissite.com/pages/forms/")
-    html = response.text
+        #with open("raw.html", "w") as file:
+            #file.write(html)
 
-    #with open("raw.html", "w") as file:
-        #file.write(html)
+        tree = HTMLParser(html)
 
-    tree = HTMLParser(html)
+        for element in tree.css("tr.team"):
+            team = {
+                "Team Name" : element.css_first("td.name").text().strip(),
+                "Year" : element.css_first("td.year").text().strip(),
+                "Wins" : element.css_first("td.wins").text().strip()
+            }
+            teams.append(team)
+        
+        if index > 24:
+            break
 
-
-
-    for element in tree.css("tr.team"):
-        team = {
-            "Team Name" : element.css_first("td.name").text().strip(),
-            "Year" : element.css_first("td.year").text().strip(),
-            "Wins" : element.css_first("td.wins").text().strip()
-        }
-        teams.append(team)
-    print(teams)
-    
+        index += 1
+        print(teams)
+        
 if __name__ == "__main__":
     main()
